150bb27b235b3b4f4493f813a190dbcebb4a64f7
TerenzTuason <terenzjetcemtuason@gmail.com>
2025-11-28 15:07:09 +0800

    Update WORKING_INSTRUCTIONS_URL in Config.js to point to the new documentation site, enhancing accessibility for users.

--- GRADES DB/Config.js
+++ GRADES DB/Config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzkXB4F2EecZ8RiK0vvI5LKIUuU15vg96SEw72nFQO9GxVslfhQRqsu5OySK8y66Qlw8w/exec",
   API_KEY: "ZGM3YjJhMzEtNWU0Yi00YjZjLWE5MzctZjIxNGU4YzRhOTEx",
-  WORKING_INSTRUCTIONS_URL: ""
+  WORKING_INSTRUCTIONS_URL: "https://grades-db.vercel.app/"
 };
 
