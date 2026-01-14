ca818905d675ca1fb06f75d741a2e21dfcfb4ad2
TerenzTuason <terenzjetcemtuason@gmail.com>
2026-01-14 08:53:39 +0800

    Update WEB_APP_URL in Config.js to the new deployment link for improved access after script deployment.

--- TEMPLATE MASTERFILE/Config.js
+++ TEMPLATE MASTERFILE/Config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // Web App URL - Update this after deploying the script as a web app
   // To deploy: Deploy > New deployment > Select type: Web app > Execute as: Me > Who has access: Anyone
   // Then copy the Web App URL (the one ending with /exec) and paste it below
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxGumZ5pJEJBD0dI9sDZkkoZ2barKVnmML3BvZqBCI_gRE_7Tt4w6W9tKTTjQqhic9d/exec", // Replace with your /exec URL after deployment
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyiwk07zzhhemEucl3uF4ijqQEbfU8Nr5SrT2Njw8Fn9AAtr7vH3Hq1eL1A48U-lrgC/exec", // Replace with your /exec URL after deployment
   
   // API Key for securing the API - This is a unique identifier
   // Keep this secret and don't share it publicly
